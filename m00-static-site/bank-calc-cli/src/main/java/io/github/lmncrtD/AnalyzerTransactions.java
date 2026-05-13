92ae4401d88c380ee343f1fb8dfcfb5f772b01e6
Lmncrt <thegord@yandex.ru>
2026-05-14 00:05:37 +0300

    Complete new tasks T1.2.

---
+++ m00-static-site/bank-calc-cli/src/main/java/io/github/lmncrtD/AnalyzerTransactions.java
@@ -0,0 +1,55 @@
+package io.github.lmncrtD;
+
+public class AnalyzerTransactions {
+    static void main(String[] args) {
+     /*
+     1. Сколько всего записей обработано.
+     2. Сколько среди них доходов, на какую общую сумму.
+     3. Сколько расходов, на какую общую сумму (положительным числом)
+     4. Сколько технических (нулевых) записей.
+     5. Итоговый баланс.
+     6. Статус баланса (Профицит/Дефицит/Нелевой)
+     */
+        int countAll = args.length;
+        double income = 0.0;
+        double outcome = 0.0;
+        int countIncome = 0;
+        int countOutcome = 0;
+        int countTech = 0;
+        String status = "";
+
+        for (String item: args) {
+            if (Double.parseDouble(item) > 0) {
+                countIncome++;
+                income += Double.parseDouble(item);
+            }
+            else if (Double.parseDouble(item) < 0) {
+                countOutcome++;
+                outcome += Double.parseDouble(item) * -1;
+            }
+            else if (Double.parseDouble(item) == 0) {
+                countTech++;
+            }
+        }
+
+        if (income - outcome == 0) {
+            status = "Нулевой";
+        } else if (income - outcome > 0) {
+            status = "Профицит";
+        } else if (income - outcome < 0) {
+            status = "Дефицит";
+        }
+
+        System.out.printf(
+                "%nОбработано записей: %d %nДоходов: %d на сумму %f %nРасходов: %d на сумму %f %nПропущено: %d%n%nБаланс: %f %nСтатус: %s%n",
+                countAll,
+                countIncome,
+                income,
+                countOutcome,
+                outcome,
+                countTech,
+                income - outcome,
+                status
+                );
+    }
+}
